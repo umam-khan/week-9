022dceda54efc147711acebd5f9d863598536e50
umam <infernape_999@hotmail.com>
2023-08-06 21:36:31 +0530

    get route changed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 app.get("/",(req, res)=>{
-    res.json({"msg":"lesgoo"});
+    res.send("lesgoo");
 })
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
